b7e62ed72eb4a5c09352ae0893b3d0e7cf64a932
Charlotte Wilson <charlottewilson@charlottes-mbp.home>
2023-02-04 18:51:58 +0000

    tomato icon avatar with spaced start stop buttons

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,7 +1,7 @@
 import Avatar from '@mui/material/Avatar';
 import { red } from '@mui/material/colors';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
 import CardHeader from '@mui/material/CardHeader';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
@@ -11,13 +11,25 @@ type TimerProps = {
 }
 const Timer = ({ minute }: TimerProps) => {
 
+    const pomodoro = () => {
+
+    }
+    const shortBreak = () => {
+
+    }
+    const longBreak = () => {
+
+    }
+    const timer = () => {
+        
+    }
+
     return (
         <div className="Container">
             <Card sx={{ maxWidth: 345 }}>
                 <CardHeader
                     avatar={
-                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            Tomato
+                        <Avatar aria-label="recipe" src="https://i.pinimg.com/originals/e1/a1/17/e1a117e9cacfb963fbb70a805f3e0109.jpg">
                         </Avatar>
                     }
                     action={
@@ -39,13 +51,9 @@ const Timer = ({ minute }: TimerProps) => {
                         Something something practise here
                     </Typography>
                 </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="share">
-                        <ShareIcon />
-                    </IconButton>
+                <CardActions className="StartStopButtons">
+                   <Button>Start</Button>
+                   <Button>Stop</Button>
                 </CardActions>
             </Card>
         </div>
