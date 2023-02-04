a6d412a68f68f99194091e4b535462d5e61d20c3
Charlotte Wilson <charlottewilson@charlottes-mbp.home>
2023-02-04 17:58:30 +0000

    example card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
 import './App.css';
-import { Card } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import { red } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
+import CardHeader from '@mui/material/CardHeader';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+
 function App() {
   return (
     <div className="App">
-      <header>Pomodoro Display here</header>
-      <Card>
-        Hello I am a card
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              Tomato
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Pomodoro"
+          subheader="Get some work done!"
+        />
+        <CardMedia
+        component="img"
+        height="194"
+        image="https://i.pinimg.com/originals/e1/a1/17/e1a117e9cacfb963fbb70a805f3e0109.jpg"
+        alt="Tomato Here pls"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+         Something something practise here 
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
       </Card>
     </div>
   );
